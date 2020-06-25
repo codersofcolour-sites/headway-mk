a00bdc741da07b5a7472bab22c5c038a15e72c91
safiaa98 <62505443+safiaa98@users.noreply.github.com>
2020-06-25 17:15:12 +0100

    added cards

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,6 +1,7 @@
 """ SteamFields live in here"""
 
 from wagtail.core import blocks 
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -21,6 +22,28 @@ class RichtextBlock(blocks.RichTextBlock):
         template = "streams/richtext_block.html"
         icon = "doc-full"
         label = "Full RichText"
+
+class CardBlock(blocks.StructBlock):
+    """Cards with image and text and button(s)."""
+
+    title = blocks.CharBlock(required=True, help_text="Add your title")
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=True)),
+                ("title", blocks.CharBlock(required=True, max_length=40)),
+                ("text", blocks.TextBlock(required=True, max_length=200)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                ("button_url", blocks.URLBlock(required=False, help_text="If the button page above is selected, that will be used first.")),
+            ]
+        )
+    )
+
+    class Meta:  # noqa
+        template = "streams/card_block.html"
+        icon = "placeholder"
+        label = "Cards"
 
 
 class SimpleRichtextBlock(blocks.RichTextBlock):
