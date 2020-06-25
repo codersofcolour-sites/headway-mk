fc2419d0de9f785b00fa4f1276c9b060f32c2a3d
safiaa98 <62505443+safiaa98@users.noreply.github.com>
2020-06-25 13:09:01 +0100

    StreamFields

--- flex/models.py
+++ flex/models.py
@@ -3,19 +3,33 @@
 
 from django.db import models
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+
+from streams import blocks 
 # Create your models here
 class FlexPage(Page):
     """Flexible Page Class"""
 
     template = "flex/flexpage.html"
-    # @todo add streamfield
-    #content = Streamfield()
+    
+    content = StreamField(
+        [ 
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+
+        ],
+        null = True, 
+        blank =True, 
+        
+    )
 
     subtitle = models.CharField(max_length =100, null=True, blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
+        StreamFieldPanel("content"),
     ]
 
     class Meta:  #noqa
