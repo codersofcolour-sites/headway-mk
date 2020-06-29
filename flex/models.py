d9492655df17bcca6dbb07c1e2b172cc99888d90
safiaa98 <62505443+safiaa98@users.noreply.github.com>
2020-06-29 16:04:35 +0100

    fixing validation error

--- flex/models.py
+++ flex/models.py
@@ -17,24 +17,66 @@ from modelcluster.fields import ParentalKey
 from streams import blocks 
 # Create your models here
 class FlexPage(Page):
-    template = "flex/flex_page.html"
+    """Flexible Page Class"""
+
+    template = "flex/flexpage.html"
+    
+    banner_title = models.CharField(max_length = 100, blank = False, null = True)
+    banner_subtitle = RichTextField(features = ["bold", "italic"], null = True, blank=True)
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image", 
+        null = True,
+        blank = True,
+        on_delete =models.SET_NULL,
+        related_name = "+",
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",  
+        null = True,
+        blank = True,
+        on_delete =models.SET_NULL,
+        related_name = "+",
+    )
+    content = StreamField([("cta", blocks.CTABlock())], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("banner_title"),
+                FieldPanel("banner_subtitle"),
+                ImageChooserPanel("banner_image"),
+                PageChooserPanel("banner_cta"),
+            ],
+            heading="Banner Options",
+        ),
+        MultiFieldPanel(
+            [InlinePanel("carousel_images", max_num=10, min_num=0, label="Image")],
+            heading="Carousel Images",
+        ),
+        StreamFieldPanel("content"),
+
+    ]
     content = StreamField(
-        [
+        [ 
             ("title_and_text", blocks.TitleAndTextBlock()),
             ("full_richtext", blocks.RichtextBlock()),
             ("simple_richtext", blocks.SimpleRichtextBlock()),
             ("cards", blocks.CardBlock()),
             ("cta", blocks.CTABlock()),
         ],
-        default="",
-        blank=True,
+        null = True, 
+        blank =True, 
+        
     )
-    subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    subtitle = models.CharField(max_length =100, null=True, blank=True)
+
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
         StreamFieldPanel("content"),
     ]
-    class Meta:  # noqa
+
+    class Meta:  #noqa
         verbose_name = "Flex Page"
         verbose_name_plural = "Flex Pages"
 
